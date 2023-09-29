31c1ae0a1b980f3003f1446ba8e46995bfa63015
Noah <scott.613865@swl.k12.oh.us>
2023-09-29 09:18:54 -0400

    yes

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,33 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scan = new Scanner(System.in);
+
+        //variables
+        int num1 = 0;
+        int num2 = 0;
+
+        System.out.print("Enter the first number: ");
+        if(scan.hasNextInt()){
+            num1 = scan.nextInt();
+        }else{
+            System.out.print("Your input was invalid.");
+            System.exit(0);
+        }
+        System.out.print("Enter the second number: ");
+        if(scan.hasNextInt()){
+            num2 = scan.nextInt();
+        }else{
+            System.out.print("Your input was invalid.");
+            System.exit(0);
+        }
+        if(num1 == num2){
+            System.out.print(num1 + " is equal to " + num2 + ".");
+        }else if(num1 > num2){
+            System.out.print(num1 + " is greater than " + num2 + ".");
+        }else if(num1 < num2){
+            System.out.print(num1 + " is less than " + num2 + ".");
+        }
     }
 }
